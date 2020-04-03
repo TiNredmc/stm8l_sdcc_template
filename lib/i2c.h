8e3c349e009c78b5fec477e89875c28d1b25180c
TINLΞTHΛX <thipok17@gmail.com>
2020-04-03 17:13:14 +0700

    I2C implementing

---
+++ lib/i2c.h
@@ -0,0 +1,17 @@
+/* I2C implementations on STM8L, All EV number is the event number. took from the Standard Periherial Library
+ * Coded by TinLethax
+ */
+ 
+#ifndef I2C_H
+#include I2C_H
+
+void i2c_init(uint8_t devID);
+uint8_t i2c_ChkEv(uint16_t I2C_Event); 
+void i2c_start();
+void i2c_stop();
+void i2c_write(uint8_t data);
+uint8_t i2c_read();
+void i2c_write_addr(uint8_t addr);
+void i2c_read_addr(uint8_t addr);
+
+#endif 
