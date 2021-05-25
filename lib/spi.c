ff2efea6334c837b5fa4fe3023978ee9f8949afa
TiNredmc <thipok17@gmail.com>
2021-05-25 22:22:46 +0700

    SPI (W.I.P) : - configurable SPI speed. - SPI_Read and SPI_Write capable of >1 byte R/W with out specify the size of data (EXPERIMENTAL). - Write then read feature implemented.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- lib/spi.c
+++ lib/spi.c
@@ -1,29 +1,66 @@
 #include "spi.h"
 #include "stm8l.h"
 
-void SPI_init() {
+/* parameter description
+ * spi_speed -> passing FSYS_DIV_x where x is between 2-256 (Sysclock divided by spi_speed = SPI clock speed)
+ */
+void SPI_Init(uint8_t spi_speed) {
 	CLK_PCKENR1 |= (1 << 4);// Enable SPI1 clock
 
 	//Enable SPI I/O GPIOs
-	PB_DDR |= (1 << 5) | (1 << 6);// SCK and MOSI pin are output
-	PB_CR1 |= (1 << 5) | (1 << 6);// with Push-Pull Mode
+	PB_DDR |= (1 << 5) | (1 << 6) | (1 << 4);// SCK, MOSI, CS pin are output
+	PB_CR1 |= (1 << 5) | (1 << 6) | (1 << 4);// with Push-Pull Mode
 	PB_CR2 |= (1 << 5) | (1 << 6);// Plus HIGH SPEEEEEEDDDDDD.
 
 	PB_DDR &= (0 << 7);// MISO pin is input
 	PB_CR1 |= (1 << 7);// with nice internal pull-up
 
-	SPI1_CR1 = (1 << SPI1_CR1_MSTR) | (1 << SPI1_CR1_BR1);
+	SPI1_CR1 = (1 << SPI1_CR1_MSTR) | (1 << SPI1_CR1_BR1) | (spi_speed << 3);
 	SPI1_CR2 = (1 << SPI1_CR2_SSM) | (1 << SPI1_CR2_SSI) | (1 << SPI1_CR2_BDM) | (1 << SPI1_CR2_BDOE);
 	SPI1_CR1 |= (1 << 6);// Enable SPI1 
 }
 
-uint8_t SPI_read() {
-    SPI_write(0xFF);
-    while (!(SPI1_SR & (1 << SPI1_SR_RXNE)));
-    return SPI1_DR;
+/* parameter descriptions
+ * *data -> pointer to the rx buffer
+ * len -> lenght (size) of receive byte(s)
+ */
+void SPI_Read(uint8_t *data, size_t len) {
+    SPI_Write((uint8_t *)0xFF, 1);
+
+	while(len--){
+		while (!(SPI1_SR & (1 << SPI1_SR_RXNE)));
+		*data++ =  SPI1_DR;
+	}
 }
 
-void SPI_write(uint8_t data) {
-    SPI1_DR = data;
-    while (!(SPI1_SR & (1 << SPI1_SR_TXE)));
+/* parameter descriptions
+ * *data -> pointer to the tx buffer
+ * len -> lenght (size) of transmit byte(s)
+ */
+void SPI_Write(uint8_t *data, size_t len) {
+	while(len--){
+		SPI1_DR = *data++;
+		while (!(SPI1_SR & (1 << SPI1_SR_TXE)));
+	}
+	while (SPI1_SR & 0x80);// wait until we are not busy 
+}
+
+/* parameter descriptions
+ * *TXbuf -> pointer to transmit buffer (auto-counted)
+ * *RXbuf -> pointer to receive buffer
+ * RXlen -> lenght (size) of receive byte(s)
+ */
+void SPI_WriteThenRead(uint8_t *TXbuf, uint8_t *RXbuf, size_t RXlen){
+	// Transmit packet to request data from slave device
+	while(*TXbuf){
+		SPI1_DR = *TXbuf++;
+		while (!(SPI1_SR & (1 << SPI1_SR_TXE)));
+	}
+	while (SPI1_SR & 0x80);// wait until we are not busy
+	
+	while(RXlen--){
+		while (!(SPI1_SR & (1 << SPI1_SR_RXNE)));
+		*RXbuf++ = SPI1_DR;
+	}
+	while (SPI1_SR & 0x80);// wait until we are not busy
 }
--- lib/spi.h
+++ lib/spi.h
@@ -2,7 +2,16 @@
 #define SPI_H
 
 #include <stdint.h>
+#include <stdio.h>
 
+#define FSYS_DIV_2	0
+#define FSYS_DIV_4	1 
+#define FSYS_DIV_8	2
+#define FSYS_DIV_16	3
+#define FSYS_DIV_32	4
+#define FSYS_DIV_64	5
+#define FSYS_DIV_128	6
+#define FSYS_DIV_256	7
 
 /* 
  * Initialize SPI in MODE1 for stm8l
@@ -11,13 +20,16 @@
  * SCK  -> PB5
  * MOSI -> PB6
  * MISO -> PB7
- * CS   -> user defined 
+ * CS   -> PB4
  */
-void SPI_init();
 
-void SPI_write(uint8_t data);
+void SPI_Init(uint8_t spi_speed);
 
-uint8_t SPI_read();
+void SPI_Write(uint8_t *data, size_t len);
+
+void SPI_Read(uint8_t *data, size_t len);
+
+void SPI_WriteThenRead(uint8_t *TXbuf, uint8_t *RXbuf, size_t RXlen);
 
 #endif /* SPI_H */
 
