0b30032d4e54edba6502cb1982877fc2e7ba4ad4
Tin LetHax <thipok17@gmail.com>
2021-08-25 23:05:11 +0700

    EPI : fixed the delay by set clock divider to 1 to get 16MHz.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- code/EPI/main.c
+++ code/EPI/main.c
@@ -6,26 +6,25 @@
 #include <EPI.h>
 #include "BMP.h"
 
-void EPI_gpio_init();
-
 void main() {
+	CLK_CKDIVR = 0x00;
 	EPI_gpio_init();
     while (1) {
 		Full_updata_setting();
 		Full_image(gImage_1);
-		delay_ms(25000);
+		delay_ms(3000);
 		//Clean
 		Full_image_Clean();
 		delay_ms(1000);
 		//Part
 		Part_updata_setting();
 		Part_image_first(gImage_2);		  //Part_image1 
-		delay_ms(15000);
+		delay_ms(1000);
 		Part_image(gImage_2,gImage_3);  //Part_image2
-		delay_ms(15000);
+		delay_ms(1000);
 
 		Part_image(gImage_3,gImage_4);	//Part_image3 
-		delay_ms(25000);
+		delay_ms(1000);
 		
     //Clean
 		Full_updata_setting();
