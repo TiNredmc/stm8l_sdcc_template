eb1596b6e3440e499c6cccd4f4f7014700177478
Tin LetHax <thipok17@gmail.com>
2021-09-19 19:53:51 +0700

    remove nonsense stuffs.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- lib/stm8l.h
+++ lib/stm8l.h
@@ -7,32 +7,6 @@
 #define _SFR_(mem_addr)                 (*(volatile uint8_t *)(0x5000 + (mem_addr)))
 #define _SFR16_(mem_addr)               (*(volatile uint16_t *)(0x5000 + (mem_addr)))
 
-typedef enum {DISABLE = 0, ENABLE = !DISABLE} FunctionalState;
-#define IS_FUNCTIONAL_STATE(STATE) (((STATE) == DISABLE) || ((STATE) == ENABLE))
-
-typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus, BitStatus, BitAction;
-
-typedef enum {ERROR = 0, SUCCESS = !ERROR} ErrorStatus;
-
-/* Exported macro ------------------------------------------------------------*/
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  The assert_param macro is used for function's parameters check.
-  * @param expr: If expr is false, it calls assert_failed function
-  *   which reports the name of the source file and the source
-  *   line number of the call that failed. 
-  *   If expr is true, it returns no value.
-  * @retval : None
-  */
-  #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-  void assert_failed(uint8_t* file, uint32_t line);
-#else
-  #define assert_param(expr) ((void)0)
-#endif /* USE_FULL_ASSERT */
-
-
 /* PORT A */
 #define PA_ODR                          _SFR_(0x00)
 #define PA_IDR                          _SFR_(0x01)
