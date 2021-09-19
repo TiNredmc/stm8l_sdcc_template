c0838d84914a1e992c43a4665d9abf5b194186e9
Tin LetHax <thipok17@gmail.com>
2021-09-19 19:54:18 +0700

    GPIO : - add example of using delay with other CPU frequency.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- code/GPIO_Blink/main.c
+++ code/GPIO_Blink/main.c
@@ -3,12 +3,14 @@
  */
 #include <stdint.h>
 #include <stm8l.h>
+// Always define before include delay.h
+#define CPU4M	//Use clock divider to get 1MHz and define this to use 1MHz delay
 #include <delay.h>
 
 #define LED_PIN     4 //PB4 
-
+ 
 void main() {
-	CLK_CKDIVR = 0x00;// Full 16Mhz, no clock divider
+	CLK_CKDIVR = 0x02;// Full 16Mhz, no clock divider
 	PB_DDR |= (1 << LED_PIN);//Direction register set 1 to the 3rd bit.
 	PB_CR1 |= (1 << LED_PIN);//Set Control register for PB4 as output.
     while (1) {
