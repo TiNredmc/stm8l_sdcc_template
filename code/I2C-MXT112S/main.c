6bb0263f648fe17e63eca4cbb7f174598a0be702
Tin LetHax <thipok17@gmail.com>
2022-08-30 07:37:38 +0700

    [W.I.P] mxt_identify() is now working properly.
    - mxt_identify() now can detect all 17 objects from mXT112S. Fixed by the byte order in I2C function
      mxt_read() and mxt_write();
    - TODO : T5 message read when interrupt goes LOW return object ID 0xFF. Need more LA probing with my Linux setup.
    
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- code/I2C-MXT112S/main.c
+++ code/I2C-MXT112S/main.c
@@ -34,6 +34,8 @@ int get_char() {
 
 // Number of elements in the Object Table
 uint8_t num_obj = 0;
+// Object query address.
+uint16_t obj_addr_cnt = 0;
 
 // Store address of each object.
 uint16_t T5_addr = 0;
@@ -65,8 +67,8 @@ void mxt_read(uint16_t addr, uint8_t *data, uint16_t len) {
 	i2c_start();// Generate start condition.
 	
 	i2c_write_addr(MXT_ADDR);// Request write to MXT112S.
-	i2c_write((uint8_t)(addr >> 8));// Write LSB byte address to MXT112S.
-	i2c_write((uint8_t)addr);// Write MSB byte address to MXT112S.
+	i2c_write((uint8_t)addr);// Write LSB byte address to MXT112S.
+	i2c_write((uint8_t)(addr >> 8));// Write MSB byte address to MXT112S.
 	
 	i2c_start();// Regenerate start condition.
 	
@@ -83,8 +85,8 @@ void mxt_write(uint16_t addr, uint8_t *data, uint8_t len) {
 	i2c_start();// Generate start condition.
 	
 	i2c_write_addr(MXT_ADDR);// Request write to MXT112S.
-	i2c_write((uint8_t)(addr >> 8));// Write LSB byte address to MXT112S.
-	i2c_write((uint8_t)addr);// Write MSB byte address to MXT112S.
+	i2c_write((uint8_t)addr);// Write LSB byte address to MXT112S.
+	i2c_write((uint8_t)(addr >>8));// Write MSB byte address to MXT112S.
 	
 	i2c_writePtr(data, len);// Write the rest of data to MXT112S.
 	
@@ -102,7 +104,7 @@ void mxt_identify() {
 
 	printf("Family ID 0x%02X\n", buf[0]);
 	printf("Variant ID 0x%02X\n", buf[1]);\
-	printf("Version : %ul.%ul\n", ((buf[2] & 0xF0) << 4), (buf[2] & 0x0F));
+	printf("Version : %u.%u\n", ((buf[2] & 0xF0) << 4), (buf[2] & 0x0F));
 	printf("Build Number : 0x0%02X\n", buf[3]);
 
 	printf("X channels : %d\n", buf[4]);
@@ -110,25 +112,33 @@ void mxt_identify() {
 
 	printf("Object table elements count : %d elements\n", buf[6]);
 
+	num_obj = buf[6];
+	obj_addr_cnt = 0x0007;
   // maXTouch can have many object. But we specifically looking for T5 A.K.A Message Processor
   // and T100 A.K.A Multitouch
   //
 	for (uint8_t i = 0; i < num_obj; i ++) {
-	mxt_read(0x0007, buf, MXT_ID_BLK_SIZ - 1);
+	mxt_read(obj_addr_cnt, buf, MXT_ID_BLK_SIZ - 1);
+
+	printf("Object : %d , ID : 0x%02X\n", i, buf[0]);
 
 	switch (buf[0]) {
 	  case 5:// T5 message processor.
 		printf("Found T5 object\n");
 		T5_addr = (buf[2] << 8) | buf[1];
+		printf("T5 object address : 0x%04X\n", T5_addr);
 		T5_msg_size = buf[3] + 1;
+		printf("T5 message size : %u\n", T5_msg_size);
 		break;
 
 	  case 9:// T9 Multi touch (Assuming the maXTouch S series have this instead of T100 from U series).
 		printf("Found T9 object\n");
 		T9_addr = (buf[2] << 8) | buf[1];
+		printf("T9 object address : 0x%04X\n", T9_addr);
 		T9_msg_size = buf[3] + 1;
 		T9_instances = buf[4] + 1;
 		T9_report_cnt = (buf[4] + 1) * buf[5];
+		printf("T9 message size : %u\nT9 report count %u\nT9 report IDs : %u\n", T9_msg_size, T9_report_cnt, buf[5]);
 		break;
 
 	//      case 100:// T100 Multi touch
@@ -139,8 +149,14 @@ void mxt_identify() {
 	//        T100_report_cnt = (buf[4] + 1) * buf[5];
 	//        break;
 	}
+	
+	obj_addr_cnt += 6;// move on to next object.
 
 	}
+	
+	// read CRC
+	mxt_read(obj_addr_cnt, buf, 3);
+	printf("CRC : 0x%02X 0x%02X 0x%02X\n", buf[0], buf[1], buf[2]);
 
 }
 
@@ -150,8 +166,11 @@ void mxt_identify() {
 // The report ID can be more than 1 Report ID.
 // Starting from 0x09 (T9 base report ID).
 void mxt_report_t9(){
-	if((PB_IDR & (1 << MXT_INT)) == 0){
-	mxt_read(T5_addr, MXT_BUF, T9_msg_size);
+	if(!(PB_IDR & (1 << MXT_INT))){
+	mxt_read(T5_addr, MXT_BUF, T5_msg_size * 2);
+
+	printf("We got data! ID : 0x%02X\n", MXT_BUF[0]);
+	
 
 	if(MXT_BUF[0] != 9){// If we didn't get T9 report (check for the T9 report ID).
 	  // Just ignore the interrupt.
@@ -166,6 +185,8 @@ void mxt_report_t9(){
 	// Convert 2 bytes to 16bit uint X and Y position 
 	xpos = (MXT_BUF[0x02] << 4) | ((MXT_BUF[0x04] >> 4) & 0x0F);
 	ypos = (MXT_BUF[0x03] << 4) | (MXT_BUF[0x04] & 0x0F);
+	
+	printf("X pos : %ul Ypos : %ul\n", xpos, ypos);
 
 	// Check for touch amplitude (Pressure?)
 	if(MXT_BUF[1] & 0x04)
@@ -181,12 +202,13 @@ void main(){
 	usart_write(0x0C);
 	i2c_init(0x69, I2C_100K);// Set I2C master address to 0x69, I2C speed to 100kHz.
 	printf("MXT112S on STM8L by TinLethax\n");
+	PB_CR1 |= 1 << MXT_INT;// Internul pull up on interrupt pin.
 	delay_ms(100);
 	mxt_identify();
 	
 	while(1){// Polling instead of interrupt (Slow down enought for the readable serial print).
-
-		delay_ms(200);
+		mxt_report_t9();
+		delay_ms(10);
 	}
 }
 
